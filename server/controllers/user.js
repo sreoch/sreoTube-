9e92ae1ec64dc6d590fa0ea2970d03a2d2466567
sreoch <scottreoch10@hotmail.co.uk>
2022-08-08 02:03:18 +0100

    finished auth handling, working on user routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,33 @@
-export const test = (req, res) => {
-	res.json('Its successful')
-}
+import { createError } from '../error.js';
+import User from '../models/User.js';
+
+export const update = async (req, res, next) => {
+	if (req.params.id === req.user.id) {
+		try {
+			const updatedUser = await User.findByIdAndUpdate(
+				req.params.id,
+				{
+					$set: req.body,
+				},
+				{ new: true } //needed for the res to show updated name
+			);
+			res.status(200).json(updatedUser);
+		} catch (err) {
+			next(err);
+		}
+	} else {
+		return next(createError(403, 'You can only update your account.'));
+	}
+};
+
+export const deleteUser = (req, res, next) => {};
+
+export const getUser = (req, res, next) => {};
+
+export const subscribe = (req, res, next) => {};
+
+export const unsubscribe = (req, res, next) => {};
+
+export const like = (req, res, next) => {};
+
+export const dislike = (req, res, next) => {};
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/users.js';
 import commentRoutes from './routes/comments.js';
 import videoRoutes from './routes/videos.js';
 import authRoutes from './routes/auth.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
@@ -20,11 +21,22 @@ const connect = () => {
 		});
 };
 
+app.use(cookieParser());
 app.use(express.json());
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/videos', videoRoutes);
 app.use('/api/comments', commentRoutes);
+
+app.use((err, req, res, next) => {
+	const status = err.status || 500;
+	const message = err.message || 'Something went wrong!';
+	return res.status(status).json({
+		success: false,
+		status,
+		message,
+	});
+});
 
 app.listen(7000, () => {
 	connect();
